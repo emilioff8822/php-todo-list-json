2832a129a0303b46a7a6fc16cf4b418bcf537955
emilioff8822 <emilioff@hotmail.it>
2023-05-18 19:11:43 +0200

    commit-index.php

--- index.php
+++ index.php
@@ -13,24 +13,37 @@
 </head>
 <body>
   <div id="app">
+    <!-- questo e' un campo input per aggiungere nuovi task. La variabile 'newTask' è definita in main.js e viene aggiornata in tempo reale grazie alla direttiva 'v-model'. Quando si preme invio (keyup.enter), il metodo 'addTask', definito nel file main.js, viene chiamato -->
+    
     <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Scrivi un task...">  
 
+    <!-- Questo è il bottone per aggiungere un nuovo task. Quando viene cliccato (@click), il metodo 'addTask', definito nel file main.js, viene chiamato -->
     <button @click="addTask">Aggiungi</button>
 
+    <!-- Questo è un elenco di task. Per ogni task nell'array 'tasks', definito in main.js, crea un elemento 'li' -->
     <ul>
+      <!-- Per ogni task in 'tasks', genera un elemento 'li' -->
       <li v-for="(task, index) in tasks" :key="index">
-      <div @click="toggleTask(task)" :class="{'done': task.completed }">
+
+        <!-- Quando si clicca su un task, il metodo 'toggleTask', definito in main.js, viene chiamato. Se il task è completato, la classe 'done' viene aggiunta a questo div grazie alla direttiva ':class' -->
+
+        <div @click="toggleTask(task)" :class="{'done': task.completed }">
+          <!-- Mostra il nome del task -->
           {{ task.task }}
         </div>
+        
+        <!-- Questo è il bottone per rimuovere un task. Quando viene cliccato, il metodo 'removeTask', definito nel file main.js, viene chiamato -->
         <button class="delete-btn" @click.stop="removeTask(task)">x</button>
       </li>
     </ul>
 
+    <!-- Se non ci sono task, mostra questo messaggio -->
     <h2 v-if="tasks.length === 0"> Non ci sono task</h2>
 
-    <!-- Messaggio di errore -->
+    <!-- Se esiste un messaggio di errore, mostralo. La variabile 'errorMessage' è definita in main.js -->
     <p v-if="errorMessage">{{ errorMessage }}</p>
   </div>
+
 
   <script src="main.js"></script>
 </body>
